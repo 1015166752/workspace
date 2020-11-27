5e53f312fac4d9eadb2f0abdad448c485b08b30c
1015166752 <1015166752@qq.com>
2020-11-27 16:09:06 +0800

    按照top规范编写

--- global.h
+++ global.h
@@ -1,4 +1,32 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-123
-#endif //GLOBAL_H
+//源程序有效注释量与源代码比例大于20％
+/*************************************
+ * Copyright (c) 2020,北京航天拓扑高科技有限责任公司
+ * All rights reserved.
+ * 
+ * Filename：global.h
+ * 
+ * Current Version: 1.1
+ * Author: Zhangsan
+ * Current Date:2020年11月17日
+ * 
+ * Replaced Version: 1.0
+ * Author: Lisi
+ * Current Date:2020年11月10日
+ * 
+ * Summary:这里写文件的摘要
+ * 
+ * **************************************/
+#ifndef GLOGAL_H
+#define GLOGAL_H
+#include <stdio.h>            /*C头文件*/
+#include <string>             /*C++头文件*/
+#include <log4cplus/logger.h> /*外部库头文件*/
+#include "user.h"             /*本工程的头文件*/
+
+/*sccp interface with sccp user primitive message name*/
+enum ESCCP_USER_PRIMITIVE{
+    N_UNITDATA_IND, 
+    N_NOTICE_IND,   
+    N_UNITDATA_REQ, 
+};
+#endif //GLOGAL_H
